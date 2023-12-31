ea68fc6a932ee8fec26d2020bf78ac554c4d73d5
Roshan Kern <roshan.kern@ucdenver.edu>
2023-12-31 07:57:24 -0700

    use numpy, get random int (railway test)

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from flask import Flask, jsonify
 import os
 
+import numpy as np
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+@app.route("/random-int")
+def random_int():
+    random_number = np.random.randint(0, 100)  # Generates a random int between 0 and 99
+    return jsonify({"random_integer": random_number})
 
 
 if __name__ == "__main__":
